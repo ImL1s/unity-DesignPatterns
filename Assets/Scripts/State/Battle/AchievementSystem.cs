f97382e2aad4705871b393571ef00b9c154017e0
ImL1s <aa22396584@gmail.com>
2017-05-30 20:33:41 +0800

    add: FacadePattern

---
+++ Assets/Scripts/State/Battle/AchievementSystem.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace YSFramework
+{
+    internal class AchievementSystem : YSObject, ISubSystem
+    {
+        private IPBaseDefenseGame pBaseDefenseGame;
+
+        public AchievementSystem(IPBaseDefenseGame pBaseDefenseGame)
+        {
+            LogD("new AchievementSystem()");
+            this.pBaseDefenseGame = pBaseDefenseGame;
+        }
+
+        public static ISubSystem Create(IPBaseDefenseGame pBaseDefenseGame)
+        {
+            return new AchievementSystem(pBaseDefenseGame);
+        }
+
+        public void Release()
+        {
+            LogD("release");
+        }
+
+        public void Update()
+        {
+            
+        }
+    }
+}
---
+++ Assets/Scripts/State/Battle/StageSystem.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace YSFramework
+{
+    internal class StageSystem : YSObject, ISubSystem
+    {
+        private IPBaseDefenseGame pBaseDefenseGame;
+
+        public StageSystem(IPBaseDefenseGame pBaseDefenseGame)
+        {
+            this.pBaseDefenseGame = pBaseDefenseGame;
+        }
+
+        public static ISubSystem Create(IPBaseDefenseGame pBaseDefenseGame)
+        {
+            return new StageSystem(pBaseDefenseGame);
+        }
+
+        public void Release()
+        {
+        }
+
+        public void Update()
+        {
+        }
+    }
+}
